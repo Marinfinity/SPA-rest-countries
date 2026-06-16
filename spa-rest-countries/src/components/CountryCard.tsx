a080849e57e07a70c6d340347f8ca41752a54774
Marinfinity <mavi.fer2006@gmail.com>
2026-06-16 19:41:47 +0200

    componente countrycard

---
+++ spa-rest-countries/src/components/CountryCard.tsx
@@ -0,0 +1,36 @@
+import type { Country } from "../types/country";
+import { useNavigate } from "react-router-dom";
+import "../index.css";
+import "../css/country-list.css"
+
+
+function CountryCard(props: { country?: Country }) {
+  const navigate = useNavigate();
+  
+
+  return (
+    <div key={props.country?.codes.ccn3} className="card" onClick={() => {
+    console.log("country:", props.country);
+    navigate(`/country/${props.country?.names.common}`, { state: { country: props.country } });
+}}>
+      <img
+        src={props.country?.flag.url_png}
+        alt={props.country?.names.common}
+        className="flag"
+      />
+
+      <h2>{props.country?.names.common}</h2>
+
+      <p><strong>Población:</strong> {props.country?.population.toLocaleString()}</p>
+      <p><strong>Región:</strong> {props.country?.region}</p>
+      <p>
+        <strong>Capital:</strong>{" "}
+        {props.country?.capitals?.[0]?.name ?? "Sin capital"}
+      </p>
+    </div>
+    )
+  
+
+}
+
+export default CountryCard
